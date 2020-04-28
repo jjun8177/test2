4291307a976d90a60a0f96c50b0fd4906b456802
Jun Young Yoon <jjun8177@naver.com>
2020-04-28 21:21:07 +0900

    git commit

---
+++ myset.py
@@ -0,0 +1,45 @@
+class Set:
+    def __init__(self, value = []):    # Constructor
+        self.data = []                 # Manages a list
+        self.concat(value)  #밑에 concat 함수 있음
+
+    def intersection(self, other):        # other is any sequence
+        res = []                       # self is the subject
+        for x in self.data:
+            if x in other:             # Pick common items
+                res.append(x)
+        return Set(res)                # Return a new Set
+
+    def union(self, other):            # other is any sequence
+        res = self.data[:]             # Copy of my list
+        for x in other:                # Add items in other
+            if not x in res:
+                res.append(x)
+        return Set(res)
+
+    def concat(self, value):
+        for x in value:                
+            if not x in self.data:     # Removes duplicates  (중복되지 않게!)
+                self.data.append(x)
+
+    def __len__(self):          return len(self.data)        # len(self)
+    def __getitem__(self, key): return self.data[key]        # self[i], self[i:j]
+    def __and__(self, other):   return self.intersection(other) # self & other
+    def __or__(self, other):    return self.union(other)     # self | other
+    def __repr__(self):         return 'Set({})'.format(repr(self.data))  
+    def __iter__(self):         return iter(self.data)       # for x in self:
+    def add(self,elem):
+        pass
+    
+    
+x = Set([1,3,5,7, 1, 3])
+y = Set([2,1,4,5,6])
+print(x, y, len(x))
+print(x.intersection(y), y.union(x))
+print(x & y, x | y)
+print(x[2], y[:2])
+for element in x:
+    print(element, end=' ')
+print()
+print(3 not in y)  # membership test
+print(list(x))   # convert to list because x is iterable
